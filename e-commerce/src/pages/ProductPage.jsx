76be0d4cf6cfe5807577914f535aeeeb6d643119
burak-senel <buurak.senel@gmail.com>
2024-04-30 21:43:24 +0300

    products detail page middle description section done

--- e-commerce/src/pages/ProductPage.jsx
+++ e-commerce/src/pages/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductDetailPage from "../layout/ProductPageLayout/ProductDetailPage";
+import ProductDetailsMiddle from "../layout/ProductPageLayout/ProductDetailsMiddle";
 
 function ProductPage() {
   return (
     <div>
       <ProductDetailPage />
+      <ProductDetailsMiddle />
     </div>
   );
 }
