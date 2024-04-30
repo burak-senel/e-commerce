e373e632e00f59d3e78671d0e1c31b848fd4b160
burak-senel <buurak.senel@gmail.com>
2024-04-30 15:58:10 +0300

    T4: Product Detail Page -Top detail section done. -Slider, and bottom components will be added

---
+++ e-commerce/src/components/ProductPageComponents/ProductPageSlider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductPageSlider() {
+  return (
+    <div>ProductPageSlider</div>
+  )
+}
+
+export default ProductPageSlider
---
+++ e-commerce/src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductDetailPage from "../layout/ProductPageLayout/ProductDetailPage";
+
+function ProductPage() {
+  return (
+    <div>
+      <ProductDetailPage />
+    </div>
+  );
+}
+
+export default ProductPage;
