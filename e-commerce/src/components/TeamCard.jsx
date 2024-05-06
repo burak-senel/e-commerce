5dfd1b1a00e5ead8c05e923b9c751e95a488caf0
burak-senel <buurak.senel@gmail.com>
2024-05-06 12:42:20 +0300

    about us page team section done

---
+++ e-commerce/src/components/TeamCard.jsx
@@ -0,0 +1,23 @@
+function TeamCard({ data, index }) {
+  return (
+    <div className="flex flex-col gap-4 tracking-[0.2px] min-h-[315px] ">
+      <img src={data.imageURL} alt={index} />
+      <div className="flex flex-col items-center gap-4 font-bold">
+        <p className="text-textcolor">{data.name}</p>
+        <p className="text-sm text-secondarygray">{data.job}</p>
+      </div>
+      <div className="flex gap-5 justify-center">
+        <a href="https://www.facebook.com/" target="blank">
+          <i className="fa-brands fa-facebook text-buttonblue text-xl"></i>{" "}
+        </a>
+        <a href="https://www.instagram.com/" target="blank">
+          <i className="fa-brands fa-instagram text-buttonblue text-xl"></i>{" "}
+        </a>
+        <a href="https://www.twitter.com/" target="blank">
+          <i className="fa-brands fa-twitter text-buttonblue text-xl"></i>{" "}
+        </a>
+      </div>
+    </div>
+  );
+}
+export default TeamCard;
--- e-commerce/src/layout/AboutPageLayout/AboutMid.jsx
+++ e-commerce/src/layout/AboutPageLayout/AboutMid.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import videoimg from "../../assets/AboutPage/aboutvideo.jpeg";
+import { useData } from "../../context/context";
+import TeamCard from "../../components/TeamCard";
 function AboutMid() {
+  const { aboutTeam } = useData();
+
   return (
     <>
       <div className="flex flex-row md:flex-col md:flex-wrap justify-between mt-4 md:text-center md:items-center md:my-6">
@@ -46,6 +50,22 @@ function AboutMid() {
         />
         <div className="bg-buttonblue w-24 h-24 rounded-full  absolute"></div>
         <i className="fa-solid fa-play absolute text-3xl text-white"></i>
+      </div>
+      <div className="flex flex-col items-center">
+        <h3 className="font-bold text-textcolor text-5xl mb-4">
+          Meet Our Team
+        </h3>
+        <p className="text-sm text-secondarygray max-w-[469px] mb-1 md:w-60 text-center">
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+        <div className="flex flex-row gap-10 py-20 md:flex-wrap">
+          {aboutTeam.members.map((item, index) => (
+            <div key={index}>
+              <TeamCard data={item} index={index} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
